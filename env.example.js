dd21b8e8e7ad2336ac31fb3f474305f46e318251
Nandagopalan - BG Mobile App Transformation <snanthagopal@gmail.com>
2017-04-21 13:27:00 +0100

    Added env file

---
+++ env.example.js
@@ -0,0 +1,7 @@
+// Checking code -- Nanda
+// Secrets for the applications. Do NOT commit any secrets to version control.
+// 1. cp env.example.js env.js
+// 2. Fill in the blanks
+
+module.exports = {
+};
